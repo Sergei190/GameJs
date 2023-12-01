04851982039343b153e7d63521b86a2ebb93a36b
Sergei190 <kudrasovcergej@gmail.com>
2023-12-01 20:22:20 +0500

    Transformed it into TypeScript and added code documentation.

--- script.js
+++ script.js
@@ -1,24 +1,20 @@
-const dino = document.getElementById("dino");
-const cactus = document.getElementById("cactus");
-
-document.addEventListener("keydown", function(event) {
+var dino = document.getElementById("dino");
+var cactus = document.getElementById("cactus");
+document.addEventListener("keydown", function (event) {
     jump();
 });
-
-function jump () {
-    if (dino.classList != "jump") {
+function jump() {
+    if (!dino.classList.contains("jump")) {
         dino.classList.add("jump");
     }
-    setTimeout( function() {
+    setTimeout(function () {
         dino.classList.remove("jump");
-    }, 300)
+    }, 300);
 }
-
-let isAlive = setInterval ( function() {
-    let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
-    let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
-
+var isAlive = setInterval(function () {
+    var dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+    var cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
     if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
-        alert("GAME OVER!")
+        alert("GAME OVER!");
     }
-}, 10)
+}, 10);
